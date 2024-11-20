d65e624ddd867d6998d8ac49f8abecef7758e0b5
Nico Afonso <nicolas.afonso@se.com>
2024-11-20 21:48:57 +0100

    first commit

---
+++ calculator.lib/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.lib
+{
+    internal class Calculator
+    {
+            public static int Add(int number1, int number2)
+            {
+                return number1 + number2;
+            }
+            public static int Aubtract(int number1, int number2)
+            {
+                return number1 - number2;
+            }
+            public static int Multiply(int number1, int number2)
+            {
+                return number1 * number2;
+            }
+            public static double Divide(double number1, double number2)
+            {
+                return ((double)number1) / ((double)number2);
+            }
+    }
+}
