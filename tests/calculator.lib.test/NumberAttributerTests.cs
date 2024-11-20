dd1dd172f761416a918de327d8cba67b10e52ab0
Nico Afonso <nicolas.afonso@correo.ugr.es>
2024-11-20 23:11:29 +0100

    The function was working also for number 1

--- tests/calculator.lib.test/NumberAttributerTests.cs
+++ tests/calculator.lib.test/NumberAttributerTests.cs
@@ -5,7 +5,8 @@ namespace calculator.lib.test
     public class NumberAttributerTests
     {
         [Theory]
-        [InlineData(2,true)]
+        [InlineData(2, true)]
+        [InlineData(1, false)]
         public void Test_IsPrime(int number, bool isItPrime)
         {
             var isPrime = NumberAttributter.IsPrime(number);
