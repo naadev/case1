5e89be06bd676259d167faab6e39160c17ef9d2f
Nico Afonso <nicolas.afonso@correo.ugr.es>
2024-11-21 20:09:46 +0100

    with new approach, it works until 25. We can find a pattern...

--- tests/calculator.lib.test/NumberAttributerTests.cs
+++ tests/calculator.lib.test/NumberAttributerTests.cs
@@ -14,6 +14,17 @@ namespace calculator.lib.test
         [InlineData(7, true)]
         [InlineData(8, false)]
         [InlineData(9, false)]
+        [InlineData(10, false)]
+        [InlineData(12, false)]
+        [InlineData(13, true)]
+        [InlineData(15, false)]
+        [InlineData(17, true)]
+        [InlineData(18, false)]
+        [InlineData(20, false)]
+        [InlineData(22, false)]
+        [InlineData(23, true)]
+        [InlineData(24, false)]
+        [InlineData(25, false)]
         public void Test_IsPrime(int number, bool isItPrime)
         {
             var isPrime = NumberAttributter.IsPrime(number);
