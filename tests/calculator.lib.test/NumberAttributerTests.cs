1a16d18bdfcc64f563d7e9c78fa55b706ddf6ab9
Nico Afonso <nicolas.afonso@correo.ugr.es>
2024-11-21 19:42:06 +0100

    With last change, we are able to make it work until number 8. Number 9 will fail...

--- tests/calculator.lib.test/NumberAttributerTests.cs
+++ tests/calculator.lib.test/NumberAttributerTests.cs
@@ -9,6 +9,11 @@ namespace calculator.lib.test
         [InlineData(1, false)]
         [InlineData(3, true)]
         [InlineData(4, false)]
+        [InlineData(5, true)]
+        [InlineData(6, false)]
+        [InlineData(7, true)]
+        [InlineData(8, false)]
+        [InlineData(9, false)]
         public void Test_IsPrime(int number, bool isItPrime)
         {
             var isPrime = NumberAttributter.IsPrime(number);
